22644f28bc5924d684e59cf9f23b6e82c7f22956
Chris Komlenic <komlenic@gmail.com>
2015-11-26 00:34:17 -0500

    Improve argparser config and help output.

--- drubs/__init__.py
+++ drubs/__init__.py
@@ -1,5 +1,6 @@
 import sys
 import argparse
+import textwrap
 from pprint import pprint
 from fabric.colors import red, yellow, green, cyan
 
@@ -11,9 +12,46 @@ def main():
     print(red("Drubs requires Python 2, version 2.7 or greater."))
     sys.exit(1)
 
-  parser = argparse.ArgumentParser()
-  parser.add_argument('action', choices=['init', 'install', 'update', 'destroy', 'enable', 'disable', 'var_dump', 'status'], help='The action to execute.')
-  parser.add_argument('nodes', nargs='+', help='one or more nodes')
+  parser = argparse.ArgumentParser(
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    description=textwrap.dedent('''\
+
+      Drubs 0.1.0
+
+        A command-line tool tool for building, deploying, and managing Drupal
+        sites across multiple servers and environments.
+
+      Available actions:
+
+        init     Create a new project with the specified nodes
+        status   Return some status information about the specified nodes
+        install  Install the project on the specified nodes (destroys any
+                   existing site)
+        update   Update the project on the specified nodes (data safe*)
+        disable  Put specified nodes into Drupal's 'maintenance mode'
+        enable   Turns off Drupal's maintenance mode (if on)
+
+      Example commands:
+
+        drubs install myserver1
+          - perform the install action on myserver1
+
+        drubs install myserver1 myserver2
+          - perform the install action on myserver1 AND myserver2
+
+        drubs install all
+          - perform the install action on all nodes found in project.yml
+
+        drubs -f /home/me/new_project/foo.yml install myserver1
+          - perform the install action on myserver1, without pwd currently
+            being /home/me/new_project, and use a project config file named
+            'foo.yml' instead of the default 'project.yml'
+
+        '''),
+    epilog='http://drubs.org'
+  )
+  parser.add_argument('action', choices=['init', 'install', 'update', 'destroy', 'enable', 'disable', 'var_dump', 'status'], help='The action to perform on the specified nodes. (see descriptions above)', metavar='action')
+  parser.add_argument('nodes', nargs='+', help='one or more nodes, or "all" which will perform the specified action on all valid nodes found in project.yml')
   parser.add_argument('-f', '--file', default='project.yml', help='path to project.yml file (not necessary if pwd contains the project.yml file)')
   parser.add_argument('-v', '--verbose', action='store_const', const='1', default='0', help='print verbose output from drush commands, if available')
   parser.add_argument('-d', '--debug', action='store_const', const='1', default='0', help='print debug output from drush commands, if available')
